20efa598bb241cbee19fc4608aed1a6e941b9bab
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-06-24 22:08:33 +0700

    Add choosing function

--- src/functions/index.js
+++ src/functions/index.js
@@ -3,16 +3,60 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.listenToFieldChange = functions.firestore
-  .document('rooms/7SMRdZZUs4Cp8GF5dWkA')
+exports.listenToRoomStateChange = functions
+  .region('asia-east2')
+  .firestore.document('rooms/{roomId}')
   .onUpdate((change, context) => {
-    const updatedField = change.after.get('endPoint');
-    const previousField = change.before.get('endPoint');
+    const roomId = context.params.roomId;
+    const oldRoomState = change.before.get('state');
+    const newRoomState = change.after.get('state');
+    const roundCount = change.after.get('roundCount');
 
-    if (updatedField !== previousField) {
-      admin.firestore().collection('rooms').doc('7SMRdZZUs4Cp8GF5dWkA').update({
-        canJoin: false,
-      });
+    // Khi room state chuyển qua choosing
+    if (
+      (oldRoomState !== newRoomState && newRoomState === 'choosing') ||
+      (oldRoomState === 'choosing' && newRoomState === 'choosing')
+    ) {
+      // Chọn từ tiếp theo
+      admin
+        .firestore()
+        .collection('rooms')
+        .doc(roomId)
+        .collection('words')
+        .orderBy('roundCount')
+        .limit(1)
+        .get()
+        .then(snapshot => {
+          admin.firestore().collection('rooms').doc(roomId).update({
+            currentWord: snapshot.docs[0].ref,
+          });
+        });
+      // Chọn người vẽ tiếp theo
+      admin
+        .firestore()
+        .collection('rooms')
+        .doc(roomId)
+        .collection('members')
+        .orderBy('roundCount')
+        .limit(1)
+        .get()
+        .then(snapshot => {
+          snapshot.docs[0].ref.update({
+            roundCount: admin.firestore.FieldValue.increment(1),
+            isChoosing: true,
+          });
+          admin
+            .firestore()
+            .collection('rooms')
+            .doc(roomId)
+            .update({
+              currentMember: snapshot.docs[0].ref,
+              roundCount: Math.max(
+                roundCount,
+                snapshot.docs[0].data().roundCount + 1,
+              ),
+            });
+        });
     }
 
     return null;
